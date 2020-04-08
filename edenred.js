1e81688a95c4eaa6c66aea84b61fad83adfefbb7
Douwe Homans <douwe@avocado.nl>
2020-04-08 15:42:45 +0200

    niet nodig:

--- edenred.js
+++ edenred.js
@@ -4,24 +4,6 @@ const expect = require('expect-puppeteer')
 const USERNAME = "####"
 const PASSWORD = "####"
 
-const escapeXpathString = str => {
-    const splitedQuotes = str.replace(/'/g, `', "'", '`);
-    return `concat('${splitedQuotes}', '')`;
-  };
-  
-const clickByText = async (page, text) => {
-    const escapedText = escapeXpathString(text);
-    const linkHandlers = await page.$$x(`//p[contains(text(), ${escapedText})]`);
-
-    if (linkHandlers.length > 0) {
-        console.log('handlers')
-        console.log(linkHandlers.length)
-        await linkHandlers[0].click();
-    } else {
-        throw new Error(`Link not found: ${text}`);
-    }
-};
-
 (async () => {
   const browser = await puppeteer.launch(
     {
@@ -43,11 +25,6 @@ const clickByText = async (page, text) => {
 
     await page.waitForNavigation()
     await page.goto('https://www.myedenred.be/#/customer/create-order-electro?product=TRE')
-
-
-    // const selector = await page.waitForSelector('.linkMenu')
-    // const createLink = await page.$('a[routerlink="menu-create-order-electro"]') 
-    // await createLink.click()
 
     const repeatButton = await page.waitForSelector('button.modalCreateOrderElectroButtonDuplicateLastOrder', {visible: true})
     await repeatButton.click()
