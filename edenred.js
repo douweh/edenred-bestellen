3471b548cee7c3acdecbc0b95dc649a17671d01d
Douwe Homans <douwe@avocado.nl>
2020-04-08 16:22:38 +0200

    async

--- edenred.js
+++ edenred.js
@@ -3,46 +3,45 @@ const config = require('./config')
 
 const USERNAME = config.username
 const PASSWORD = config.password
-
-(async () => {
+let run = async function(){
   const browser = await puppeteer.launch(
     {
         headless: false
     }
-  );
-    const page = await browser.newPage();
-    await page.goto('https://www.myedenred.be/');
-   
-    await page.waitForSelector('div.profil', {visible: true})
-    const profiles = await page.$$('div.profil')
-    await profiles[1].click()
-    await page.waitForNavigation()
+  )
+  const page = await browser.newPage();
+  await page.goto('https://www.myedenred.be/');
 
-    await page.type('#username', USERNAME);
-    await page.type('#password', PASSWORD);
+  await page.waitForSelector('div.profil', {visible: true})
+  const profiles = await page.$$('div.profil')
+  await profiles[1].click()
+  await page.waitForNavigation()
 
-    await page.$eval('#form', form => form.submit());
+  await page.type('#username', USERNAME);
+  await page.type('#password', PASSWORD);
 
-    await page.waitForNavigation()
-    await page.goto('https://www.myedenred.be/#/customer/create-order-electro?product=TRE')
+  await page.$eval('#form', form => form.submit());
 
-    const repeatButton = await page.waitForSelector('button.modalCreateOrderElectroButtonDuplicateLastOrder', {visible: true})
-    await repeatButton.click()
+  await page.waitForNavigation()
+  await page.goto('https://www.myedenred.be/#/customer/create-order-electro?product=TRE')
 
-    // Wachten tot bestelling geladen is
-    await page.waitForSelector('.titreNumber', {visible: true})
-        
-    const bestelling = await page.waitForXPath('//*[@id="createOrderPageStep3"]/div/div[1]/div[3]/table/tbody/tr', {visible: true})
-    const cellen = await bestelling.$$eval('td', tds => { return tds.map(td => td.textContent).join(', ') })
-    const aantal = await bestelling.$eval('.titreNumber', input => { return input.value })
-    const waarde = await bestelling.$eval('.titreValue', input => { return input.value })
-    console.log(`${aantal} bonnen van ${waarde} euro voor: ${cellen}`)
- 
-    const submitButton = await page.$('#validateCommand')
-    submitButton.click()
+  const repeatButton = await page.waitForSelector('button.modalCreateOrderElectroButtonDuplicateLastOrder', {visible: true})
+  await repeatButton.click()
 
-    const submitDefinitiefButton = await page.waitForXPath('//*[@id="validateOrderPage"]/section[2]/div/div[3]/div[2]/button', {visible: true})
-    submitDefinitiefButton.click();
-  
-  //await browser.close();
-})();
+  // Wachten tot bestelling geladen is
+  await page.waitForSelector('.titreNumber', {visible: true})
+      
+  const bestelling = await page.waitForXPath('//*[@id="createOrderPageStep3"]/div/div[1]/div[3]/table/tbody/tr', {visible: true})
+  const cellen = await bestelling.$$eval('td', tds => { return tds.map(td => td.textContent).join(', ') })
+  const aantal = await bestelling.$eval('.titreNumber', input => { return input.value })
+  const waarde = await bestelling.$eval('.titreValue', input => { return input.value })
+  console.log(`${aantal} bonnen van ${waarde} euro voor: ${cellen}`)
+
+  const submitButton = await page.$('#validateCommand')
+  submitButton.click()
+
+  const submitDefinitiefButton = await page.waitForXPath('//*[@id="validateOrderPage"]/section[2]/div/div[3]/div[2]/button', {visible: true})
+  submitDefinitiefButton.click();
+};
+
+run()
